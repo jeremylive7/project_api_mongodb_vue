961d45a64bfdadbafce97228549186f6f352696b
Jeremy Live_live_live <jeremylive096@gmail.com>
2021-06-20 11:14:26 -0700

    1

---
+++ src/components/CardList.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div class="row">
+        <div class="col-12">
+            Card
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import {onMounted} from 'vue'
+import {useStore} from 'vuex'
+export default {
+    setup(){
+        const store = useStore()
+        onMounted(() => {
+            console.log(store.state.productos)
+            store.dispatch('getProductos')
+        }),
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    state: {
+        productos: []
+    }
   },
   mutations: {
+    setProductos(state, payload) {
+      state.productos = payload
+    }
   },
   actions: {
+    async getProductos ({ commit }) {
+      try {
+        const res = await fetch('')
+        const data = await res.json()
+        commit('setProductos', data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
   modules: {
   }
